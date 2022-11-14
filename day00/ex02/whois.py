b5ce2a0d101f8a6176fb276f6042cd29b8b354a8
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 18:15:02 +0100

    optimization

--- day00/ex02/whois.py
+++ day00/ex02/whois.py
@@ -1,12 +1,12 @@
 from sys import argv
 
 if __name__ == '__main__' :
-    if len(argv) == 1 :
-        print("Usage: python whois.py [number]")
-    elif len(argv) > 2 :
+    if len(argv) > 2 :
         print("AssertionError: more than one argument are provided")
-    else :
-        try:
-            print(f"I'm {'Zero' if int(argv[1]) == 0 else 'Odd' if int(argv[1]) % 2 == 1 else 'Even'}.")
-        except:
-            print("AssertionError: argument is not an integer")
+        exit(1)
+    try:
+        print(f"I'm {'Zero' if not int(argv[1]) else 'Odd' if int(argv[1]) % 2 else 'Even'}.")
+    except:
+        if len(argv) == 1 :     print("Usage: python whois.py [number]")       
+        else :                  print("AssertionError: argument is not an integer")
+        exit(1)
