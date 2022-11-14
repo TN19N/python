33976bb44de6022b4814b2a60de6c6a8f50982de
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 14:52:22 +0100

    finish ex08

---
+++ day00/ex08/sos.py
@@ -0,0 +1,28 @@
+import sys
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ' ':'/'}
+
+if __name__ == '__main__' :
+    if len(sys.argv) == 1:
+        print('ERROR')
+        exit(1)
+
+    allstring = ' '.join([arg for arg in sys.argv[1:]])
+
+    if not all(c.isalnum() or c.isspace() for c in allstring) :
+        print('ERROR')
+        exit(1)
+
+    print(' '.join([MORSE_CODE_DICT[c.upper()] for c in allstring]))
