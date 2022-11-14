9cc2b4f188fa77477eabc2614b64c7faeee7c231
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 19:43:01 +0100

    optimization

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -18,26 +18,22 @@ cookbook = {
 
 def printMenu() :
     print ('''\
-\
 List of available option:
     1: Add a recipe
     2: Delete a recipe
     3: Print a recipe
     4: Print the cookbook
-    5: Quit
-\
-''', end='')
+    5: Quit\
+''')
 
 def printRecipe(recipe) :
     if recipe in cookbook :
         print(f'''
-\
 Recipe for {recipe}:
     Ingredients list: {cookbook[recipe]['ingredients']}
     To be eaten for {cookbook[recipe]['meal']}.
-    Takes {cookbook[recipe]['prep_time']} minutes of cooking.
-\
-''', end = '')
+    Takes {cookbook[recipe]['prep_time']} minutes of cooking.\
+''')
     else :
         print("\nrecipe don't exist")
 
@@ -58,19 +54,16 @@ def addRecipe() :
     print('>>> Enter ingredients:')
     ingredients = []
     while True :
-        ingredient = input()
-        if ingredient and not ingredient.isspace() :
-            ingredients.append(ingredient)
-        else :
-            break
+        try:    ingredient = input()
+        except: break
+        if ingredient and not ingredient.isspace() :    ingredients.append(ingredient)
+        else :  break
     meal = input('>>> Enter a meal type:\n')
     while True :
-        prep_time = input('>>> Enter a preparation time:\n')
-        if not prep_time.isdigit() :
-            print('pleas enter a number !!')
-        else :
-            break
-    cookbook[name] = { "ingredients": ingredients, "meal" : meal, "prep_time" : int(prep_time) }
+        try:    prep_time = int(input('>>> Enter a preparation time:\n'))
+        except: continue
+        if prep_time >= 0 : break
+    cookbook[name] = { "ingredients": ingredients, "meal" : meal, "prep_time" : prep_time }
     print ('\nNew recipe has been added')
 
 if __name__ == '__main__' :
@@ -79,19 +72,14 @@ if __name__ == '__main__' :
     printMenu()
 
     while True :
-        print('\nPlease select an option:')
-        option = input('>> ')
-        if option == '1' :
-            addRecipe()
-        elif option == '2':
-            deleteRecipe(input('\nPlease enter a recipe name to delet:\n>> '))
-        elif option == '3':
-            printRecipe(input('\nPlease enter a recipe name to get its details:\n>> '))
-        elif option == '4':
-            printCookbook()
-        elif option == '5':
-            print('EXITED')
-            break
+        try: option = input('\nPlease select an option:\n>> ')
+        except: break
+        if option == '1' :  addRecipe()
+        elif option == '2': deleteRecipe(input('\nPlease enter a recipe name to delet:\n>> '))
+        elif option == '3': printRecipe(input('\nPlease enter a recipe name to get its details:\n>> '))
+        elif option == '4': printCookbook()
+        elif option == '5': break
         else :
             print('\nSorry, this option does not exist.')
             printMenu()
+    print('EXITED')
