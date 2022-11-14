57ff4dc94b0d28a30ce4db12ae9f9d27b294bdd1
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 19:05:33 +0100

    optimization

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -1,25 +1,26 @@
-import sys
+from sys import argv
 
 if __name__ == '__main__' :
-    if len(sys.argv) > 3 :
+    if len(argv) > 3 :
         print('AssertionError: too many arguments')
         exit(1)
-    if len(sys.argv) < 3 :
+    if len(argv) < 3 :
         print('Usage: python operations.py <number1> <number2>\nExample:\n\tpython operations.py 10 3')
         exit(1)
-    if not sys.argv[1].isdigit() or not sys.argv[2].isdigit() :
+
+    A = 0
+    B = 0
+    try:
+        A = int(argv[1])
+        B = int(argv[2])
+    except:
         print('AssertionError: only integers')
         exit(1)
 
-    A = int(sys.argv[1])
-    B = int(sys.argv[2])
-
-    print('Sum:        ', A + B)
-    print('Difference: ', A - B)
-    print('Product:    ', A * B)
-    if B == 0 :
-        print('Quotient:    ERROR (division by zero)')
-        print('Remainder:   ERROR (modulo by zero)')
-    else :
-        print('Quotient:   ', A / B)
-        print('Remainder:  ', A % B)
+    print(f'''\
+Sum:        {A + B}
+Difference: {A - B}
+Product:    {A * B} 
+Quotient:   {'ERROR (division by zero)' if B == 0 else A / B}
+Remainder:  {'ERROR (modulo by zero)' if B == 0 else A % B}\
+''')
