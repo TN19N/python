79e35d0f2fe43c348c65488a7ab7d27a15133760
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 15:12:12 +0100

    finish ex09

---
+++ day00/ex09/guess.py
@@ -0,0 +1,38 @@
+from random import randint
+
+if __name__ == '__main__' :
+    print('''\
+\
+This is an interactive guessing game!
+You have to enter a number between 1 and 99 to find out the secret number.
+Type 'exit' to end the game.
+Good luck!
+\
+    ''')
+
+    number = randint(1, 99)
+    attempts = 0
+
+    while True :
+        cmd = input("What's your guess between 1 and 99?\n>> ")
+        attempts += 1
+        if cmd == 'exit' :
+            print('Goodbye!')
+            break
+        elif not cmd.isdigit() :
+            print("That's not a number.")
+        elif int(cmd) > 99 or int(cmd) < 1 :
+            print('number sholde be between 1 and 99 !')
+        else :
+            if int(cmd) > number :
+                print ('Too high!')
+            elif int(cmd) < number :
+                print('Too low!')
+            else:
+                if number == 42 :
+                    print('The answer to the ultimate question of life, the universe and everything is 42.')
+                if attempts == 1:
+                    print ('Congratulations! You got it on your first try!')
+                else:
+                    print("Congratulations, you've got it!", f"You won in {attempts} attempts!" , sep='\n')
+                break
