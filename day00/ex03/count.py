dcb022c933d5418f6bb2151ba042a92193e6b4f8
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 18:55:10 +0100

    optimization

--- day00/ex03/count.py
+++ day00/ex03/count.py
@@ -1,46 +1,28 @@
-import string
-import sys
+from string import punctuation
+from sys import argv
 
-def text_analyzer(*arg):
-    """ This function counts the number of upper characters, lower characters,\n punctuation and spaces in a given text. """
-    upperLetters = 0
-    lowerLetters = 0
-    punctuations = 0
-    spaces = 0
+def text_analyzer(string = None):
+    """\n\tThis function counts the number of upper characters, lower characters,\n\tpunctuation and spaces in a given text."""
 
-    buffer = ""
-    if len(arg) == 1 :
-        if type(arg[0]) != str :
-            print('AssertionError: argument is not a string')
-            return
-        buffer = arg[0]
-    elif len(arg) == 0 :
-        print('What is the text to analyze?')
-        buffer = input(">> ")
+    if string == None :
+        try: string = input('What is the text to analyze?\n>> ')
+        except: exit(0)
+    if type(string) != str :
+        print('AssertionError: argument is not a string')
     else:
-        print("Usage: text_analyzer([string])")
-        return
-
-    for c in buffer :
-        if c.isupper() == True :
-            upperLetters += 1
-        elif c.islower() == True :
-            lowerLetters += 1
-        elif c.isspace() == True :
-            spaces += 1
-        elif c in string.punctuation :
-            punctuations += 1
-
-    print(f'The text contains {len(buffer)} character(s):')
-    print(f'- {upperLetters} upper letter(s)')
-    print(f'- {lowerLetters} lower letter(s)')
-    print(f'- {punctuations} punctuation mark(s)')
-    print(f'- {spaces} space(s)')
+        print(f'''\
+\
+The text contains {len(string)} character(s):
+- {len([c for c in string if c.isupper()])} upper letter(s)
+- {len([c for c in string if c.islower()])} lower letter(s)
+- {len([c for c in string if c in punctuation])} punctuation mark(s)
+- {len([c for c in string if c.isspace()])} space(s)\
+\
+''')
 
 if __name__ == '__main__' :
-    if len(sys.argv) == 2 :
-        text_analyzer(sys.argv[1])
-    elif len(sys.argv) == 1 :
-        text_analyzer()
+    if len(argv) < 3 :
+        text_analyzer(''.join(argv[1:]))
     else :
-        print("Usage: text_analyzer([string])")
+        print("Usage: python count.py [string](optional)")
+        exit(1)
