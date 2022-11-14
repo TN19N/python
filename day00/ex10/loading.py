1bc8609510cce18cb4c52693bdfccae69a1fc54d
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 19:59:05 +0100

    optimization

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -1,14 +1,12 @@
 from time import time, sleep 
-from tqdm import tqdm
 
 def ft_progress(listy) :
     startTime = time()
     for i in listy :
-        lodingTime = time() - startTime
         lodingPercentage = int((i + 1) / len(listy) * 100)
         lodingBar = ('='*int(lodingPercentage*20/100) + '>')
         est = (len(listy) - (i + 1)) * 0.01
-        print(f'ETA: {est:.2f}s [ {lodingPercentage}%] [{lodingBar:21}] {i + 1}/{len(listy)} | elapsed time {lodingTime:.2f}s', end='\r')
+        print(f'ETA: {est:.2f}s [ {lodingPercentage}%] [{lodingBar:21}] {i + 1}/{len(listy)} | elapsed time {time() - startTime:.2f}s', end='\r')
         yield i 
 
 if __name__ == '__main__' :
