2a34676b06e31ed045483c9232e9c3c439aa867d
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-14 18:16:50 +0100

    optimization

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -3,5 +3,5 @@ from sys import argv
 if __name__ == '__main__' :
     if len(argv) == 1 :
         print("Usage: python exec.py [ARG 1] [ARG2] ... ")
-    else :
-        print(f"{' '.join(argv[1:])[::-1].swapcase()}")
+        exit(1)
+    print(f"{' '.join(argv[1:])[::-1].swapcase()}")
