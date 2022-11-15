893ac3db68889bc50a34c01f0773e828117aae24
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-15 10:16:30 +0100

    finish ex01

---
+++ day01/ex01/game.py
@@ -0,0 +1,18 @@
+class GotCharacter :
+    def __init__(self, first_name = None, is_alive = True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+class Stark(GotCharacter) :
+    """A class representing the Stark family. Or when bad things happen to good people."""
+
+    def __init__(self, first_name = None, is_alive = True):
+        super().__init__(first_name, is_alive)
+        self.family_name = 'Stark'
+        self.house_words = "Winter is Coming"
+
+    def print_house_words(self) :
+        print(self.house_words)
+
+    def die(self) :
+        self.is_alive = False
