0d0ee7d96dda9b66b316465669bd2ab9e318f4dc
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-15 17:09:35 +0100

    finish ex04

---
+++ day01/ex04/eval.py
@@ -0,0 +1,19 @@
+class Evaluator :
+
+    def zip_evaluate(coefs, words) :
+        if type(coefs) != list or type(words) != list or len(coefs) != len(words) \
+            or not all(type(c) == float and type(w) == str for (c, w) in zip(coefs, words)) :
+            return -1
+        res = 0
+        for (c, w) in zip(coefs, words) :
+            res += c * len(w)
+        return res
+
+    def enumerate_evaluate(coefs, words) :
+        if type(coefs) != list or type(words) != list or len(coefs) != len(words) \
+            or not all(type(c) == float and type(w) == str for (c, w) in zip(coefs, words)) :
+            return -1
+        res = 0
+        for (i, w) in enumerate(words) :
+            res += coefs[i] * len(w)
+        return res
