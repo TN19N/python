5fd6f1620cc915056b8af3688d61997fa90edbf4
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-17 14:00:38 +0100

    finish ex01

---
+++ day03/ex01/ImageProcessor.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+class ImageProcessor :
+    
+    def load(self, path) :
+        try :
+            arr = mpimg.imread(path)
+            print(f'Loading image of dimensions {arr.shape[0]} x {arr.shape[0]}')
+            return arr
+        except Exception as error:
+            print(f'Exception: {type(error).__name__} -- strerror: {error.strerror if hasattr(error, "strerror") is True else None}')
+            return None
+    
+    def display(self, array) :
+        try :
+            plt.imshow(array)
+            plt.show()
+        except Exception as error:
+            print(f'Exception: {type(error).__name__} -- strerror: {error.strerror if hasattr(error, "strerror") is True else None}')
