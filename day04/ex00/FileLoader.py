8b2689364021f33e0150fb6fc9cc1954e64ae230
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2022-11-19 10:49:15 +0100

    finish ex00

---
+++ day04/ex00/FileLoader.py
@@ -0,0 +1,23 @@
+import pandas
+
+class FileLoader :
+    
+    def load(self, path) :
+        try :
+            data = pandas.read_csv(path)
+            print(f'Loading dataset of dimensions {" x ".join(str(num) for num in data.shape)}')
+            return data
+        except :
+            return None
+    
+    def display(self, df, n) :
+        try:
+            print(type(n))
+            if n >= 0 :
+                print('----------+---------')    
+                print(df.head(n))
+            else :
+                print('----------*---------')
+                print(df.tail(-1 * n))
+        except:
+            print('None')
